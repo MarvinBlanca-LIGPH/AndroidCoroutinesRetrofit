53f8b8ecd7dfa18e0d4c61cbb92029b843faf1c2
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-01-18 12:25:49 +0800

    Initial commit

---
+++ app/src/main/java/com/devtides/androidcoroutinesretrofit/model/Country.kt
@@ -0,0 +1,9 @@
+package com.devtides.androidcoroutinesretrofit.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Country(
+    val countryName: String?,
+    val capital: String?,
+    val flag: String?
+)
